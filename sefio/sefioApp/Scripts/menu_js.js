77545722d567a8aeaea89a805485f5c0ad790bf1
RashadWestSide <faradzhov84@gmail.com>
2019-01-31 02:55:03 +0300

    upd

---
+++ sefio/sefioApp/Scripts/menu_js.js
@@ -0,0 +1,29 @@
+﻿//$(function(){
+//    var flag = 0;
+//    $('.menu-icon').on('click', function(e){
+//      e.preventDefault();
+//      if(flag === 0 ){
+//        $('.menu-icon').addClass("active");
+//        $('.menu').addClass('visible'); 
+//        flag = 1;
+//      }else {
+//        $('.menu-icon').removeClass("active");
+//        $('.menu').removeClass('visible');
+//        flag = 0;
+//      }
+//   });
+//});
+
+$(document).ready(function () {
+    $(document).delegate('.open', 'click', function (event) {
+        $(this).addClass('oppenned');
+        event.stopPropagation();
+    })
+    $(document).delegate('body', 'click', function (event) {
+        $('.open').removeClass('oppenned');
+    })
+    $(document).delegate('.cls', 'click', function (event) {
+        $('.open').removeClass('oppenned');
+        event.stopPropagation();
+    });
+});
